7fcceaf72fbdd3a28c96f98bd00c928fffd9b5ae
Jack-Moss <Jack-Moss-Work@hotmail.co.uk>
2024-02-09 08:26:10 +0000

    adding some basics

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use linux_embedded_hal::{I2cdev, Delay};
 use i2cdev::linux::LinuxI2CError;
 use circular_buffer::CircularBuffer;
 
+
 fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
   let i2c = I2cdev::new("/dev/i2c-1")
           .map_err(Mpu6050Error::I2c)?;
@@ -17,18 +18,18 @@ fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
 
 
 
-    // get roll and pitch estimate
-    let acc = mpu.get_acc_angles()?;
-    println!("r/p: {:?}", acc);
+    // get roll and pitch estimate? is there a yaw here too?
 
+    let acc = mpu.get_acc_angles()?;
+    println!("r/p: {:?}", ang);
     // get gyro data, scaled with sensitivity 
     let gyro = mpu.get_gyro()?;
     println!("gyro: {:?}", gyro);
-
     // get accelerometer data, scaled with sensitivity
     let acc = mpu.get_acc()?;
     println!("acc: {:?}", acc);
-
+    let map: ???
+    buffer.push_back({acc ,gyro, ang});
     //Do some unholy maths to get a general 
     //apply a kalman filter to moderate noise
     //Do a current accerlation check
