4894bdd47a644775da4d462ba7fcbf977b34e469
Jack-Moss <Jack-Moss-Work@hotmail.co.uk>
2024-02-14 08:41:01 +0000

    figuring out structs

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,26 @@ use linux_embedded_hal::{I2cdev, Delay};
 use i2cdev::linux::LinuxI2CError;
 use circular_buffer::CircularBuffer;
 
-
+struct Data_point{
+  accel: u32,
+  gyro: u32,
+  magnetic: u32,
+}
+// I think I need a custom implementation of the circular buffer that allows me to store arrays rather than singular numbers as I want
+// {acc,gyro,mag} readings at each point
 fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
   let i2c = I2cdev::new("/dev/i2c-1")
           .map_err(Mpu6050Error::I2c)?;
 
   let mut delay = Delay;
   let mut mpu = Mpu6050::new(i2c);
+  const inital_datapoint = Data_point{
+    accel: 0,
+    gyro:0,
+    magnetic:0,
+  }
   //create buffer with len 100
-  let mut buffer = CircularBuffer::<100,u32>::new();
+  let mut buffer: CircularBuffer<100, Data_point> = CircularBuffer::<100,inital_datapoint>::new();
   mpu.init(&mut delay)?;
 
   loop {
