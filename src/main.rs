6a60d3570e1c3955c5d0ac9fc9b464d238ec104b
Jack-Moss <Jack-Moss-Work@hotmail.co.uk>
2024-03-12 08:20:36 +0000

    debugging and refactorin

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,24 @@
-use mpu6050::*;
-use linux_embedded_hal::{I2cdev, Delay};
-use i2cdev::linux::LinuxI2CError;
 use circular_buffer::CircularBuffer;
-use i2c-config::Configured_i2c;
-use std::collections::VecDeque
+use ::mycrate::i2c_config;
+use std::collections::VecDeque;
 // is it possible to simulate an i2c connection for this project? would be a lot easier to develop if so.
 
-
-
-struct DataPoint{
-  accel: f32,
-  gyro: f32,
-  magnetic: f32,
-}
-
-impl DataPoint{
-  //holyshit this would let you set custom processes if you wanted so 
-//if I wanted to I could just pull raw data and have it edited at the datapoint level which is 
-//pretty darned wild. I like that a lot not sure if its more efficent though
-//
-}
 
 
 
 // I think I need a custom implementation of the circular buffer that allows me to store arrays rather than singular numbers as I want
 // {acc,gyro,mag} readings at each point
-fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
+fn main() {
   //create buffer with len 100
-  let mut buffer: CircularBuffer<100, DataPoint> = CircularBuffer::<100,DataPoint>::new();
+  let mut buffer: CircularBuffer<100, T> = CircularBuffer::<100,T>::new();
   let mut dump_stack;
-  let deque: VecDeque<DataPoint> = VecDeque::with_capacity(300);
+  let deque: VecDeque<T> = VecDeque::with_capacity(300);
 
   let dataloop = loop {
 
 
 
     // loop of adding items to the circular buffer
-    let datapoint = DataPoint{
-      magnetic: mpu.get_acc_angles(),
-      gyro:mpu.get_gyro(),
-      accel:mpu.get_acc(),
-    };
-    buffer.push_back(datapoint);
-
-
-
-
-
-    // let acc = mpu.get_acc_angles()?;
-    // println!("r/p: {:?}", ang);
-    // let gyro = mpu.get_gyro()?;
-    // println!("gyro: {:?}", gyro);
-    // let acc = mpu.get_acc()?;
-    // println!("acc: {:?}", acc);
 
   };
 }
