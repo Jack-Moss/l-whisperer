f5dd729933346f8d1076b71c779649325e1f7de0
Jack-Moss <Jack-Moss-Work@hotmail.co.uk>
2024-03-13 08:44:11 +0000

    adding cunning plans

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use circular_buffer::CircularBuffer;
-use ::mycrate::i2c_config;
 use std::collections::VecDeque;
 // is it possible to simulate an i2c connection for this project? would be a lot easier to develop if so.
 struct DataPoint{
@@ -14,16 +13,16 @@ struct DataPoint{
 // {acc,gyro,mag} readings at each point
 fn main() {
   //create buffer with len 100
-  let mut buffer: CircularBuffer<100, DataPoint> = CircularBuffer::<100,DataPoint>::new();
-  let dump_stack: Vec<DataPoint> = Vec::with_capacity(1000);
-  let deque: VecDeque<DataPoint> = VecDeque::with_capacity(300);
+  let mut buffer: CircularBuffer<300, DataPoint> = CircularBuffer::<300,DataPoint>::new();
+  let deque: VecDeque<DataPoint> = VecDeque::with_capacity(1300);
+  //Start loop and keep the led turning on and off while its workinginto (maybe in a seperate thread? or just flash a bunch at the start)
+  //while not detecting sudden acceleration on any axis
+  //Read from i2c connection 
+  //add to circular buffer
+  //if sudden acceleration detected then switch to dumping into the dequeue until its full
+  //dump the ciruclar buffer into the front of the deque in reverse order so its all alighned. 
+  //end process and turn off led.
 
-  let dataloop = loop {mut
-    mut
 
-
-    // loop of adding items to the circular buffer
-
-  };
 }
 
