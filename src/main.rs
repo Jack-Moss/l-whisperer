974413d1406a5a19f92da1e426073b63de895ff9
Jack-Moss <Jack-Moss-Work@hotmail.co.uk>
2024-03-18 08:59:21 +0000

    starting to convert python code to rust

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ struct DataPoint{
   magnetic: f32,
 }
 
-
-
 // I think I need a custom implementation of the circular buffer that allows me to store arrays rather than singular numbers as I want
 // {acc,gyro,mag} readings at each point
 fn main() {
@@ -21,51 +19,9 @@ fn main() {
   let deque: VecDeque<DataPoint> = VecDeque::with_capacity(1300);
 
 
-  let data_loop = loop {
-    //Start loop and keep the led turning on and off while its workinginto (maybe in a seperate thread? or just flash a bunch at the start)
-
-  };
-  //while not detecting sudden acceleration on any axis
-  //Read from i2c connection 
-  //add to circular buffer
-  //if sudden acceleration detected then switch to dumping into the dequeue until its full
-  //dump the ciruclar buffer into the front of the deque in reverse order so its all alighned. 
-  //end process and turn off led.
-
-
 }
 
-// fn i2c_init(){
-//   let i2c = I2cdev::new("/dev/i2c-1")
-//   .map_err(Mpu6050Error::I2c)?;
-  
-//   let mut delay = Delay;
-//   let mut mpu = Mpu6050::new(i2c);
-//   mpu.init(&mut delay)?;
-//   let datapoint = DataPoint{
-//       magnetic: mpu.get_acc_angles(),
-//       gyro:mpu.get_gyro(),
-//       accel:mpu.get_acc(),
-//     };fn i2c_init(){
-//   let i2c = I2cdev::new("/dev/i2c-1")
-//   .map_err(Mpu6050Error::I2c)?;
-  
-//   let mut delay = Delay;
-//   let mut mpu = Mpu6050::new(i2c);
-//   mpu.init(&mut delay)?;
-//   let datapoint = DataPoint{
-//       magnetic: mpu.get_acc_angles(),
-//       gyro:mpu.get_gyro(),
-//       accel:mpu.get_acc(),
-//     };
-//     buffer.push_back(datapoint);
-
-
-// }
-//     buffer.push_back(datapoint);
-
-
-// }
+fn create_buffer()->
 
 fn calculate_kn(climber_weight: i32, max_speed: i32) ->i32 {
   climber_weight * max_speed
